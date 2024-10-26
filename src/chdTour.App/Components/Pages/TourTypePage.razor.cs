1f57e232f94b1b928a63ada2e7c1c9ef29929cee
Christoph Decker <C.Decker@metalldeutsch.com>
2024-10-26 13:57:18 +0200

    save

---
+++ src/chdTour.App/Components/Pages/TourTypePage.razor.cs
@@ -0,0 +1,23 @@
+using chd.UI.Base.Components.Base;
+using chdTour.App.Components.Base;
+using chdTour.Contracts.Constants;
+using chdTour.DataAccess.Contracts.Domain;
+using chdTour.DataAccess.Contracts.Interfaces.Repositories;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace chdTour.App.Components.Pages
+{
+    public partial class TourTypePage : BaseSearchForm<ITourTypeRepository, TourType>
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            this.Title = PageTitleConstants.TourTypes;
+
+            this._items = await this._repository.Where(x => true).ToListAsync(this._cts.Token);
+
+            await base.OnInitializedAsync();
+        }
+
+    }
+}
